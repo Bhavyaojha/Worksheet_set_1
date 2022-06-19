89cf6aaa13a2a32f999aa11615179eba5dae3129
Bhavyaojha <99117736+Bhavyaojha@users.noreply.github.com>
2022-06-19 13:35:14 +0530

    Add files via upload

---
+++ PYTHON.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      BHAVYA OJHA
+#
+# Created:     26-01-2022
+# Copyright:   (c) BHAVYA OJHA 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+superhero = input(" what is your superhero name ?")
+print(superhero)
